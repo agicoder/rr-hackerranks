68052233f2eb40d2485e53bd462ba66487d61769
Nguyen Trung Tuyen <tuyennt@fingroup.vn>
2018-06-11 17:13:03 +0700

    arrow funciton

--- src/App.js
+++ src/App.js
@@ -2,19 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+  title: 'React',
+  url: 'https://facebook.github.io/react/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://github.com/reactjs/redux',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ];
+
 class App extends Component {
   render() {
-    const helloWorld = "Road to learn Reactjs"
-  //const and let
-  //p15 should use const over var
-  const halloWorld = {
-    text: 'Welcome to the Road to learn React'
-    };
-  // can change value inside  
-  halloWorld.text = 'Bye Bye React';
     return (
       <div className="App">
-        <h2> {helloWorld} </h2>
+        {list.map((item) => {
+          return (
+            <div key={item.objectID}>
+              <span>
+              <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root'));
+// Hot Module Replacement ( HMR) reload react app but don't do  page refresh
+if (module.hot) {
+    module.hot.accept();
+    }
 registerServiceWorker();
