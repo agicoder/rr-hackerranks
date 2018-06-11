0f9b49fbf315393ce8e28855d43de96f695c4d28
Nguyen Trung Tuyen <tuyennt@fingroup.vn>
2018-06-11 17:16:40 +0700

    arrow function without { return(

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map((item) => {
-          return (
+        {list.map((item) =>
             <div key={item.objectID}>
               <span>
               <a href={item.url}>{item.title}</a>
@@ -35,8 +34,7 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
             </div>
-          );
-        })}
+        )}
       </div>
     );
   }
