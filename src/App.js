591ca0777702995539bfd9352b78920c6b7df6d5
Nguyen Trung Tuyen <tuyennt@fingroup.vn>
2018-06-12 14:49:28 +0700

    ES6 destructuring

--- src/App.js
+++ src/App.js
@@ -22,6 +22,17 @@ const list = [
   },
 ];
 
+function isSearchedES5(searchTerm) {
+  return function (item) {
+    // some condition which returns true or false
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+    // return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1; ES5
+  }
+}
+
+const isSearched = searchTerm => item =>
+item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -29,7 +40,9 @@ class App extends Component {
       list: list,
       name: name,
       age: age,
+      searchTerm: '',
     };
+    this.onSearchChange = this.onSearchChange.bind(this);
     // this.onDismiss = this.onDismiss.bind(this);
   }
   // onDismiss(id) {
@@ -37,6 +50,10 @@ class App extends Component {
   //     return item.objectID !== id;
   //   });
   // }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
 
   onDismiss(id) {
     function isNotId(item) {
@@ -47,13 +64,19 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
+      
       <div className="App">
-        {this.state.list.map(item => {
+        <form>
+          <input type="text" onChange={this.onSearchChange} />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item => {
+        {/* {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => { */}
+        {/* {this.state.list.map(item => { */}
           const onHandleDismiss = () =>
             this.onDismiss(item.objectID);
-
-          return(
+          return (
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title} </a>
@@ -64,9 +87,9 @@ class App extends Component {
               <span>{this.state.name} </span>
               <span>{this.state.age} </span>
               <span>
-                <button onClick={console.log(item.objectID)}
-                
-                type="button">
+                <button onClick={() => this.onDismiss(item.objectID)}
+
+                  type="button">
                   Dismiss
                 </button>
               </span>
